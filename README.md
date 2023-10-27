914ed6c68da9b4f60a7e5622a51c45b7e9221536
bescoss <pablo.bescos@sarria.salesians.cat>
2023-10-27 15:40:55 +0200

    new_form

---
+++ README.md
@@ -0,0 +1,3 @@
+# new_form_flutter
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Recuperar el valor d' 'un camp de text',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return scaffold(
+      appBar: AppBar(
+        
+      )
+      body: Padding(
+
+      )
+      floatingActionButton: FloatingActionButton()
+    )
+  }
+}
